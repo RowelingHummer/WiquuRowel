82fab437f10c8c5e09bc806014f6585d06923682
RowelingHummer <kevin.lopez@rowel.co>
2018-02-02 17:19:41 -0500

    se genera la nueva pagina de index, se crea la clase header que permite generar los header dependiendo de que pagina se encuentra( aun se esta trabajando en ella).

--- estructura/home.php
+++ estructura/home.php
@@ -4,6 +4,11 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php 
+$nombreUsuario = "Margarita";
+$idPagina = 1;
+$urlImagenPerfil = "./imagenes/Avatar.jpg";
+?>
 <html>
     <head>
         <meta charset="UTF-8">
--- home.php
+++ home.php
@@ -5,5 +5,5 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-include './estructura/inicio.php';
+include './estructura/home.php';
 ?>
